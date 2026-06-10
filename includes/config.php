d500c0f7daae133bdfdae3c65d10aa06f23958b8
Våmosss <jvrlaws@gmail.com>
2026-06-10 20:22:38 +0700

    Testing ges

--- includes/config.php
+++ includes/config.php
@@ -13,3 +13,22 @@ if (!$conn) {
     die(print_r(sqlsrv_errors(), true));
 }
 ?>
+
+
+//Ini punya kalian kalo kalian engga bisa jalanin program karna ada notif enga bisa jalan
+
+<!-- <?php
+$serverName = "."; 
+$connectionOptions = [
+    "Database" => "hoopball",
+    "Uid" => "", 
+    "PWD" => "",
+    "TrustServerCertificate" => true
+];
+
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+if (!$conn) {
+    die(print_r(sqlsrv_errors(), true));
+}
+?> -->
