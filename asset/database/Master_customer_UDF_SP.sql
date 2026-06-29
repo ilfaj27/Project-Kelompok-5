4ccfa284f2d010eb03f70af90f8161754fa0b8ba
ilfaj <ilfaj.alnur27@gmail.com>
2026-06-29 15:15:42 +0700

    tambahkan udf dahsboard csutomer

--- asset/database/Master_customer_UDF_SP.sql
+++ asset/database/Master_customer_UDF_SP.sql
@@ -183,15 +183,3 @@ BEGIN
       AND Is_Deleted = 0;
 END;
 GO
-
-
--- 1. UDF: Mengambil jumlah lapangan aktif (Scalar)
-CREATE OR ALTER FUNCTION dbo.fn_GetActiveCourtCount()
-RETURNS INT
-AS
-BEGIN
-    DECLARE @Count INT;
-    SELECT @Count = COUNT(*) FROM dbo.Lapangan WHERE Status = 1 AND Is_Deleted = 0;
-    RETURN ISNULL(@Count, 0);
-END;
-GO
