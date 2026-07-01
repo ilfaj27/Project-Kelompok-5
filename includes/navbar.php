a8ba2273a47f7adb6c6e18e6e4c46c814ba8fa98
ilfaj <ilfaj.alnur27@gmail.com>
2026-07-01 23:40:06 +0700

    foldering

---
+++ includes/navbar.php
@@ -0,0 +1,58 @@
+<?php
+// Pastikan prefix folder terdefinisi dengan aman
+$prefix = isset($path_prefix) ? $path_prefix : '';
+?>
+<nav class="navbar">
+    <a href="<?= $prefix ?>index.php" class="logo">
+        <img src="<?= $prefix ?>asset/image/logo2.png" alt="HoopBall">
+    </a>
+    <div class="nav-menu">
+        <a href="<?= $prefix ?>customer/booking_customer.php">Booking</a>
+        <a href="<?= $prefix ?>customer/pembatalan_customer.php">Pembatalan</a>
+        <a href="<?= $prefix ?>customer/langganan_customer.php">Member</a>
+        <a href="<?= $prefix ?>customer/pembelian_alat.php">Pembelian</a>
+    </div>
+    <div class="nav-btns">
+        <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true): ?>
+            <!-- TAMPILAN JIKA SUDAH LOGIN (DROPDOWN PROFIL) -->
+            <div class="profile-dropdown-container">
+                <button class="btn-profile-trigger" id="profileTrigger">
+                    <i class="fa-solid fa-circle-user profile-icon-orange"></i>
+                    <span class="profile-trigger-name"><?= htmlspecialchars($_SESSION['Nama_Customer'] ?? 'User') ?></span>
+                    <i class="fa-solid fa-chevron-down chevron-icon"></i>
+                </button>
+
+                <div class="profile-dropdown-menu" id="profileDropdownMenu">
+                    <div class="profile-dropdown-header">
+                        <span class="user-fullname"><?= htmlspecialchars($_SESSION['Nama_Customer'] ?? 'User') ?></span>
+                        <span class="user-role">CUSTOMER</span>
+                    </div>
+
+                    <div class="profile-dropdown-divider"></div>
+
+                    <ul class="profile-dropdown-list">
+                        <li>
+                            <a href="<?= $prefix ?>profile/profile_customer.php">
+                                <i class="fa-regular fa-user"></i> Profil Saya
+                            </a>
+                        </li>
+                    </ul>
+
+                    <div class="profile-dropdown-divider"></div>
+
+                    <ul class="profile-dropdown-list">
+                        <li>
+                            <a href="<?= $prefix ?>login/logout.php">
+                                <i class="fa-solid fa-arrow-right-from-bracket"></i> Keluar
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        <?php else: ?>
+            <!-- TAMPILAN JIKA BELUM LOGIN -->
+            <a href="<?= $prefix ?>login/login.php" class="btn-login">Masuk</a>
+            <a href="<?= $prefix ?>login/register.php" class="btn-join">Daftar Sekarang</a>
+        <?php endif; ?>
+    </div>
+</nav>
